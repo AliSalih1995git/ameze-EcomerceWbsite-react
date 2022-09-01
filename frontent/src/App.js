bc14aa6acb11f9f210539c33e37b1ea441db2fb2
AliSalih1995git <kvaliswalihtnl@gmail.com>
2022-09-01 22:38:39 +0530

    crete-react-app

---
+++ frontent/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amaze</a>
+      </header>
+      <main>List Products </main>
+    </div>
+  );
+}
+
+export default App;
